af708784c67d6b1c07bb3059694da9de41ce0612
Elisabeth Hutzel <thesnowdragoon@gmail.com>
2013-08-24 22:50:45 -0400

    Added Jigglypuff animations. Added Sandshrew and Jigglypuff to the game.

---
+++ PokemonSmash/Data/Pokemon/027_Sandshrew/sandshrew.py
@@ -0,0 +1,30 @@
+from Pokemon import *;
+from Move import *;
+
+Sandshrew = Pokemon.Add("sandshrew");
+Sandshrew.DisplayName = "Sandshrew";
+Sandshrew.Animation = "Pokemon/027_Sandshrew/sandshrew"
+Sandshrew.HP = 50;
+Sandshrew.Attack = 75;
+Sandshrew.Defense = 85;
+Sandshrew.SpecialAttack = 20;
+Sandshrew.SpecialDefense = 30;
+Sandshrew.Speed = 40;
+
+Sandshrew.Width = .5;
+Sandshrew.Height = .5;
+Sandshrew.Weight = 12.0;
+
+
+Sandshrew.SetMix("walk", "idle", 0.6);
+Sandshrew.SetMix("walk", "jump", 0.2);
+
+Sandshrew.SetMix("jump", "walk", 0.1);
+Sandshrew.SetMix("jump", "idle", 0.1);
+
+Sandshrew.SetMix("idle", "jump", 0.2);
+Sandshrew.SetMix("idle", "walk", 0.4);
+
+Sandshrew.SetMix("idle", "dead", 1);
+Sandshrew.SetMix("walk", "dead", 1);
+Sandshrew.SetMix("jump", "dead", 1);
---
+++ PokemonSmash/Data/Pokemon/039_Jigglypuff/jigglypuff.py
@@ -0,0 +1,30 @@
+from Pokemon import *;
+from Move import *;
+
+Jigglypuff = Pokemon.Add("jigglypuff");
+Jigglypuff.DisplayName = "Jigglypuff";
+Jigglypuff.Animation = "Pokemon/027_Jigglypuff/jigglypuff"
+Jigglypuff.HP = 115;
+Jigglypuff.Attack = 45;
+Jigglypuff.Defense = 20;
+Jigglypuff.SpecialAttack = 45;
+Jigglypuff.SpecialDefense = 25;
+Jigglypuff.Speed = 20;
+
+Jigglypuff.Width = .5;
+Jigglypuff.Height = .5;
+Jigglypuff.Weight = 5.5;
+
+
+Jigglypuff.SetMix("walk", "idle", 0.6);
+Jigglypuff.SetMix("walk", "jump", 0.2);
+
+Jigglypuff.SetMix("jump", "walk", 0.1);
+Jigglypuff.SetMix("jump", "idle", 0.1);
+
+Jigglypuff.SetMix("idle", "jump", 0.2);
+Jigglypuff.SetMix("idle", "walk", 0.4);
+
+Jigglypuff.SetMix("idle", "dead", 1);
+Jigglypuff.SetMix("walk", "dead", 1);
+Jigglypuff.SetMix("jump", "dead", 1);
