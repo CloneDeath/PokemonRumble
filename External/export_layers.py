977374d6f30df330e11dbb5bd944574d67430b76
Nicholas Rodine <halfofastaple@gmail.com>
2013-08-28 19:46:11 -0400

    Split up pokemon types. Fixed export layers script (again). Fixed growlithe's output.

--- External/export_layers.py
+++ External/export_layers.py
@@ -20,20 +20,20 @@ gettext.install("gimp20-python", gimp.locale_directory, unicode=True)
 #sys.stdout = open( 'c:\\temp\\gimpstdout.txt', 'w');
 
 #===============================================================================
-#import gtk;
-#def MessageBox(message, title = None): # most for debugging
-#	dialog = gtk.MessageDialog(
-#		None,
-#		gtk.DIALOG_MODAL,
-#		gtk.MESSAGE_INFO,
-#		gtk.BUTTONS_OK,
-#		message
-#	)
-#	if not title:
-#		title = "Message"
-#	dialog.set_title(title)
-#	dialog.run()
-#	dialog.destroy()
+import gtk;
+def MessageBox(message, title = None): # most for debugging
+	dialog = gtk.MessageDialog(
+		None,
+		gtk.DIALOG_MODAL,
+		gtk.MESSAGE_INFO,
+		gtk.BUTTONS_OK,
+		message
+	)
+	if not title:
+		title = "Message"
+	dialog.set_title(title)
+	dialog.run()
+	dialog.destroy()
 #===============================================================================
 
 def format_filename(img, layer):
@@ -50,57 +50,45 @@ def format_filename(img, layer):
 	
 	filename = group + normal_regex.sub('_', layername) + '.png'
 	return filename
+	
+def export_layer(image, layer, path, remove_offsets, crop):
+	dupe = image.duplicate()
+	layer.visible = 1;
+	filename = format_filename(dupe, layer)
+	fullpath = os.path.join(path, filename);
+	if not os.path.exists(os.path.dirname(fullpath)):
+		os.makedirs(os.path.dirname(fullpath))
+		
+	if (remove_offsets):
+		layer.set_offsets(0, 0) 
+	if (crop):
+		pdb.plug_in_zealouscrop(dupe, layer)
+	pdb.file_png_save(dupe, layer, fullpath, filename, 0, 9, 1, 1, 1, 1, 1)
+	gimp.delete(dupe)
 
-def get_layers(layers, only_visible):
+def get_layers(image, layers, path, only_visible, remove_offsets, crop):
 	version = gimp.version[0:2]
-	result = []
 	for layer in layers:
 		if version[0] >= 2 and version[1] >= 8: #version 2.8 and up
 			if pdb.gimp_item_is_group(layer):
-				result += get_layers(layer.children, only_visible)
+				get_layers(image, layer.children, path, only_visible, remove_offsets, crop)
 			else:
 				if only_visible:
 					if layer.visible:
-						layer.visible = 0
-						result.append(layer)
+						export_layer(image, layer, path, remove_offsets, crop);
 				else:
-					layer.visible = 0
-					result.append(layer)
+					export_layer(image, layer, path, remove_offsets, crop);
 		else: #version below 2.8
 			if only_visible:
 				if layer.visible:
-					layer.visible = 0
-					result.append(layer)
+					export_layer(image, layer, path, remove_offsets, crop);
 			else:
-				layer.visible = 0
-				result.append(layer)
-			
-	return result
+				export_layer(image, layer, path, remove_offsets, crop);
 
 
-def export_layers(img, drw, path, only_visible=True, flatten=False, remove_offsets=False, crop=False):
+def export_layers(img, drw, path, only_visible=True, remove_offsets=True, crop=True):
 	dupe = img.duplicate()
-	layers = get_layers(dupe.layers, only_visible)
-		
-	for layer in dupe.layers:
-		layer.visible = 0;
-		
-	for layer in layers:
-		#MessageBox(layer.name);
-		layer.visible = 1;
-		filename = format_filename(img, layer)
-		fullpath = os.path.join(path, filename);
-		if not os.path.exists(os.path.dirname(fullpath)):
-			os.makedirs(os.path.dirname(fullpath))
-			
-		if (flatten):
-			dupe.flatten()
-		if (remove_offsets):
-			layer.set_offsets(0, 0) 
-		if (crop):
-			pdb.plug_in_zealouscrop(dupe, layer)
-		pdb.file_png_save(dupe, layer, fullpath, filename, 0, 9, 1, 1, 1, 1, 1)
-	gimp.delete(dupe);
+	get_layers(img, img.layers, path, only_visible, remove_offsets, crop)
 			
 register(
 	proc_name=("python-fu-export-layers"),
@@ -116,9 +104,8 @@ register(
 		(PF_DRAWABLE, "drw", "Drawable", None),
 		(PF_DIRNAME, "path", "Save PNGs here", os.getcwd()),
 		(PF_BOOL, "only_visible", "Only Visible Layers?", True),
-		(PF_BOOL, "flatten", "Flatten Images?", False),
-		(PF_BOOL, "remove_offsets", "Remove Offsets?", False),
-		(PF_BOOL, "crop", "Zealous Crop", False),
+		(PF_BOOL, "remove_offsets", "Remove Offsets?", True),
+		(PF_BOOL, "crop", "Zealous Crop", True),
 		],
 	results=[],
 	function=(export_layers), 
