3a46433208575b47afd70f65012997a61f2cec6c
Nicholas Rodine <halfofastaple@gmail.com>
2013-08-30 17:05:41 -0400

    Added pidgey.

---
+++ PokemonSmash/Data/Pokemon/016_Pidgey/pidgey.py
@@ -0,0 +1,36 @@
+import Pokemon;
+import Move;
+import Color;
+import Type;
+
+Pidgey = Pokemon.Add("pidgey");
+Pidgey.DisplayName = "Pidgey";
+Pidgey.Animation = "Pokemon/010_Pidgey/pidgey"
+Pidgey.HP = 40;
+Pidgey.Attack = 45;
+Pidgey.Defense = 40;
+Pidgey.SpecialAttack = 35;
+Pidgey.SpecialDefense = 35;
+Pidgey.Speed = 56;
+Pidgey.Color = Color.Brown;
+Pidgey.PrimaryType = Type.Normal;
+Pidgey.SecondaryType = Type.Flying;
+
+Pidgey.Width = .5;
+Pidgey.Height =  .5;
+Pidgey.Weight = 1.8;
+
+Pidgey.CanLearn("tackle");
+Pidgey.CanLearn("sandattack");
+Pidgey.CanLearn("gust");
+Pidgey.CanLearn("quickattack");
+Pidgey.CanLearn("whirlwind");
+Pidgey.CanLearn("twister");
+Pidgey.CanLearn("featherdance");
+Pidgey.CanLearn("agility");
+Pidgey.CanLearn("wingattack");
+Pidgey.CanLearn("roost");
+Pidgey.CanLearn("tailwind");
+Pidgey.CanLearn("mirrormove");
+Pidgey.CanLearn("airslash");
+Pidgey.CanLearn("hurricane");
